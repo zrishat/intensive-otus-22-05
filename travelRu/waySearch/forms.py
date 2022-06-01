88de27bbb6ca02481e9b0982897525eeb0e24492
Zeilanda <83118578+Zeilanda@users.noreply.github.com>
2022-06-01 20:19:55 +0300

    form added

---
+++ travelRu/waySearch/forms.py
@@ -0,0 +1,13 @@
+# from django import forms
+# from django.contrib.admin.widgets import AdminDateWidget
+#
+#
+# class SearchForm(forms.Form):
+#     departure_city = forms.CharField(label="departure_city", max_length=30)
+#     arrival_city = forms.CharField(label="arrival_city", max_length=30)
+#     departure_date = forms.DateField(label="departure_date",
+#                                      input_formats=('%d.%m.%Y', '%d/%m/%Y', '%d-%m-%Y'),
+#                                      widget=AdminDateWidget)
+#     arrival_date = forms.DateField(label="arrival_date",
+#                                    input_formats=('%d.%m.%Y', '%d/%m/%Y', '%d-%m-%Y'),
+#                                    widget=AdminDateWidget)
