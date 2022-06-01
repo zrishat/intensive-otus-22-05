6f79e627522a805bef254aa280eb9f229bf9393b
Rishat Zainullin <r.zainullin@omp.ru>
2022-06-01 22:55:13 +0300

    Update form template

--- travelRu/waySearch/views.py
+++ travelRu/waySearch/views.py
@@ -9,33 +9,28 @@ def index(request):
 
 
 def search_avia_page(request):
+    data_info = {"data": ""}
     if request.method == "POST":
-        data = request.POST.items()
-    print(dict(data))
-    print(request.POST)
-    return render(request, 'search_avia.html', {'data': data})
-    # return render(request, "search_avia.html")
-
-
-def search_avia_tickets(request):
-    if request.method == "POST":
-        print('POST')
-    else:
-        print('Another')
-    data = request.POST.items()
-    print(data)
-    return render(request, 'search_avia.html', {'data': data})
-
-    # url = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
-    # headers = {'Content-Type': 'application/json'}
-    # params = {'origin': 'MOW', 'destination': 'LED', 'currency': 'rub', 'departure_at': '2022-06-10',
-    #           'sorting': 'price', 'direct': 'true', 'token': TOKEN_AVIASALES}
-    # json_to_download = requests.get(url, headers=headers, params=params).json()
-    #
-    # print(f'link_got: {json_to_download}')
-    #
-    # return render(request, 'search_aviatickets.html', context=json_to_download)
+        url = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
+        headers = {'Content-Type': 'application/json'}
+        data = request.POST
+        origin = data['departure_city']
+        destination = data['arrival_city']
+        departure_at = data['departure_date']
+        return_at = data['arrival_date']
+        # direct — получить рейсы без пересадок. По умолчанию false
+        params = {'origin': from_city_to_code(origin), 'destination': from_city_to_code(destination), 'currency': 'rub',
+                  'departure_at': departure_at,
+                  'return_at': return_at, 'sorting': 'price', 'direct': 'true', 'token': TOKEN_AVIASALES}
+        # params = {'origin': 'MOW', 'destination': 'LED', 'currency': 'rub', 'departure_at': '2022-06-10',
+        #           'sorting': 'price', 'direct': 'true', 'token': TOKEN_AVIASALES}
+        data_info = requests.get(url, headers=headers, params=params).json()
+    return render(request, 'search_avia.html', data_info)
 
 
 def search_train_page(request):
     return render(request, "search_train.html")
+
+
+def from_city_to_code(code):
+    return code
