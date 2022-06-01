08075c590367dc6181d6df4ba53b0d59428567fa
Zeilanda <83118578+Zeilanda@users.noreply.github.com>
2022-06-01 20:19:29 +0300

    make form django

--- travelRu/waySearch/views.py
+++ travelRu/waySearch/views.py
@@ -9,19 +9,32 @@ def index(request):
 
 
 def search_avia_page(request):
-    return render(request, "search_avia.html")
+    if request.method == "POST":
+        data = request.POST.items()
+    print(dict(data))
+    print(request.POST)
+    return render(request, 'search_avia.html', {'data': data})
+    # return render(request, "search_avia.html")
 
 
 def search_avia_tickets(request):
-    url = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
-    headers = {'Content-Type': 'application/json'}
-    params = {'origin': 'MOW', 'destination': 'LED', 'currency': 'rub', 'departure_at': '2022-06-10',
-              'sorting': 'price', 'direct': 'true', 'token': TOKEN_AVIASALES}
-    json_to_download = requests.get(url, headers=headers, params=params).json()
+    if request.method == "POST":
+        print('POST')
+    else:
+        print('Another')
+    data = request.POST.items()
+    print(data)
+    return render(request, 'search_avia.html', {'data': data})
 
-    print(f'link_got: {json_to_download}')
-
-    return render(request, 'search_aviatickets.html', context=json_to_download)
+    # url = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
+    # headers = {'Content-Type': 'application/json'}
+    # params = {'origin': 'MOW', 'destination': 'LED', 'currency': 'rub', 'departure_at': '2022-06-10',
+    #           'sorting': 'price', 'direct': 'true', 'token': TOKEN_AVIASALES}
+    # json_to_download = requests.get(url, headers=headers, params=params).json()
+    #
+    # print(f'link_got: {json_to_download}')
+    #
+    # return render(request, 'search_aviatickets.html', context=json_to_download)
 
 
 def search_train_page(request):
