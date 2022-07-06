58e3b8c6f5946d4a8f07a7c5fbb58840e666edb0
Zeilanda <83118578+Zeilanda@users.noreply.github.com>
2022-07-06 14:13:44 +0300

    added stars

--- travelru/search_hotels/templatetags/calculation_tag.py
+++ travelru/search_hotels/templatetags/calculation_tag.py
@@ -16,3 +16,13 @@ def total_cost(price, nights, persons_count=1):
     """
     # print(type(price), type(nights), type(persons_count))
     return price * nights * persons_count
+
+
+@register.simple_tag
+def print_multiple(value, count):
+    """Print a value multiple times."""
+    return ' '.join([value] * count)
+
+@register.filter(name='times')
+def times(number):
+    return range(number)
