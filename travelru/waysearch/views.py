e027cfbd356792605df5e0444f25392fc9b829e9
alex <tsoialex82@gmail.com>
2022-07-08 15:46:43 +0000

    Avia: fixed moscow123-bug Avia: flights data from get_request_answer processes in a separate function before rendering

--- travelru/waysearch/views.py
+++ travelru/waysearch/views.py
@@ -26,9 +26,9 @@ def search_avia_page(request):
         search_avia = WaysearchForm(request.POST)
         url = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
         headers = {'Content-Type': 'application/json'}
-        data = request.POST
-        origin = data['departure_city']
-        destination = data['arrival_city']
+        form_data = request.POST
+        origin = form_data['departure_city']
+        destination = form_data['arrival_city']
 
         city_1, city_2 = False, False
         for city_iata in IATA:
@@ -43,8 +43,8 @@ def search_avia_page(request):
         if not city_1 or not city_2:
             return render(request, 'search_avia.html', {'form': search_avia})
 
-        departure_at = data['departure_date']
-        return_at = data['arrival_date']
+        departure_at = form_data['departure_date']
+        return_at = form_data['arrival_date']
         if departure_at > return_at:
             search_avia.add_error('departure_date', f'Дата возвращения должна быть позже даты вылета')
             return render(request, 'search_avia.html', {'form': search_avia})
@@ -58,11 +58,12 @@ def search_avia_page(request):
                   'direct': 'true',
                   'token': TOKEN_AVIASALES}
         try:
-            data_info = requests.get(url, headers=headers, params=params).json()
+            get_answer = requests.get(url, headers=headers, params=params).json()
         except Exception:  # pylint: disable=broad-except
             search_avia.add_error('departure_city', 'Сервис API недоступен.')
             return render(request, 'search_avia.html', {'form': search_avia})
-        data_info['data_f'] = data
+        data_info = extract_data_from_answer(get_answer)
+        data_info['data_f'] = form_data
     data_info["iata"] = IATA
     return render(request, 'search_avia.html', {'form': search_avia, "data": data_info})
 
@@ -83,7 +84,33 @@ def from_city_to_code(city):
             airport = city.split()[-1].strip("()")
             if iata_tag['name'] == airport:
                 return iata_tag['code']
+            else:
+                return 'MOW'
         else:
             if iata_tag['city_name'] == city:
                 return iata_tag['code']
     return ""
+
+def extract_data_from_answer(answer):
+    data_info = {}
+    if answer['success']:
+        data_info['success'] = True
+        flights = answer['data']
+        for flight in flights:
+            for iata_tag in IATA:
+                if iata_tag['code'] == flight['origin_airport']:
+                    if iata_tag['city_name'] == "Москва":
+                        flight['depature_city_text'] = f"Москва ({iata_tag['name']})"
+                    else:
+                        flight['depature_city_text'] = iata_tag['city_name']
+                if iata_tag['code'] == flight['destination_airport']:
+                    if iata_tag['city_name'] == "Москва":
+                        flight['arrival_city_text'] = f"Москва ({iata_tag['name']})"
+                    else:
+                        flight['arrival_city_text'] = iata_tag['city_name']
+        data_info['data'] = flights
+        return data_info
+
+
+
+
